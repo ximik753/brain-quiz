85c98b3259457353a8dd3ff94a64abfae04f005e
ximik_753 <sankovdiman@mail.ru>
2020-10-04 11:47:08 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': 'off',
+    'indent': 'off',
+    'semi': ['error', 'never'],
+    'comma-dangle': ['error', 'never'],
+    'curly': 'off'
+  }
+}
---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {StyleSheet, View, Text } from 'react-native'
+
+const App = () => {
+    return (
+        <View>
+            <Text>Brain Quiz</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default App
