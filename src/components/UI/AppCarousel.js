f0508d10028ab5824fe280ff7c4b49062d1126a4
ximik_753 <sankovdiman@mail.ru>
2020-10-08 21:10:34 +0300

    adding a carousel component

---
+++ src/components/UI/AppCarousel.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { StyleSheet, Image } from 'react-native'
+import Carousel from 'react-native-snap-carousel'
+
+const AppCarousel = () => {
+    const items = [
+        <Image style={styles.item} source={require('../../assets/images/main/carousel/todo.png')}/>,
+        <Image style={styles.item} source={require('../../assets/images/main/carousel/timetable.png')}/>,
+        <Image style={styles.item} source={require('../../assets/images/main/carousel/update.png')}/>
+    ]
+
+    const renderItems = ({ item }) => item
+
+    return (
+        <Carousel
+            layout="default"
+            data={items}
+            sliderWidth={290}
+            itemWidth={230}
+            firstItem={1}
+            renderItem={renderItems}
+            contentContainerCustomStyle={styles.wrapper}
+        />
+    )
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        marginTop: 25
+    },
+    item: {
+        width: 230,
+        height: 127
+    }
+})
+
+export default AppCarousel
