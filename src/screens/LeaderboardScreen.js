c19fe6e471310b8a55cae0017500bddb142628c9
ximik_753 <sankovdiman@mail.ru>
2020-10-13 21:19:47 +0300

    the receiving and rendering of the leaderboard from the server

--- src/screens/LeaderboardScreen.js
+++ src/screens/LeaderboardScreen.js
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { Fragment, useCallback, useEffect, useState } from 'react'
 import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import AppCard from '../components/UI/Cards/AppCard'
 import AppCardLeaderboardItem from '../components/UI/Cards/AppCardLeaderboardItem'
 import { fonts } from '../utils/fonts'
 import { colors } from '../utils/colors'
+import { useHttp } from '../hooks/http.hook'
 
 const LeaderboardScreen = () => {
+    const { get } = useHttp()
+    const [items, setItems] = useState([])
+
+    const fetchData = useCallback(async () => await get('/top', true), [get])
+
+    useEffect(() => {
+        fetchData()
+            .then(data => setItems(data))
+            .catch(e => console.log(e))
+    }, [])
+
     return (
         <ScrollView contentContainerStyle={styles.wrapper}>
             <View style={styles.container}>
                 <Text style={styles.title}>Таблица лидеров</Text>
                 <AppCard>
-                    <AppCardLeaderboardItem/>
-                    <AppCardLeaderboardItem/>
-                    <AppCardLeaderboardItem/>
-                    <AppCardLeaderboardItem/>
-                    <AppCardLeaderboardItem/>
+                    {
+                        items.map(({ name, stats, avatar }, index) => (
+                            <Fragment key={name}>
+                                <AppCardLeaderboardItem
+                                    name={name}
+                                    avatar={avatar}
+                                    iq={stats.iq}
+                                    position={index + 1}
+                                />
+                            </Fragment>
+                        ))
+                    }
                 </AppCard>
             </View>
         </ScrollView>
