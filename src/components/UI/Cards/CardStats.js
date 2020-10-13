2a20ecc58844399c1a6499d18995dce1e032f856
ximik_753 <sankovdiman@mail.ru>
2020-10-13 17:48:17 +0300

    Adding custom icons

--- src/components/UI/Cards/CardStats.js
+++ src/components/UI/Cards/CardStats.js
@@ -1,23 +1,38 @@
 import React from 'react'
-import { View, StyleSheet, Image, Text } from 'react-native'
+import { View, StyleSheet, Image, Text, TouchableNativeFeedback } from 'react-native'
 import AppCardStatsItem from './AppCardStatsItem'
 import AppCard from './AppCard'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import { fonts } from '../../../utils/fonts'
 import { colors } from '../../../utils/colors'
 import { useSelector } from 'react-redux'
+import { avatars } from '../../../utils/avatars'
 
 const CardStats = () => {
-    const { name, stats } = useSelector(state => state.user)
+    const { name, stats, icon } = useSelector(state => state.user)
     const { gameTotal, gameWinner, iq } = stats
 
     return (
         <AppCard style={styles.cardStats}>
             <Image
-                style={styles.userIcon}
-                source={{ uri: 'https://avatars3.githubusercontent.com/u/34097384?s=460&u=3b74d5a77b93a2f4ce535f68ec659f5e92772bc0&v=4' }}
+                style={styles.avatar}
+                source={avatars[icon]}
+                resizeMode="contain"
             />
-            <Text style={styles.userName}>{name}</Text>
+            <View style={[
+                { transform: [{ translateX: 10 }, { translateY: -5 }] }
+            ]}>
+                <TouchableNativeFeedback
+                    onPress={() => console.log(123)}
+                >
+                    <MaterialIcons
+                        name="settings"
+                        color={colors.defaultFontColor}
+                        size={19}
+                    />
+                </TouchableNativeFeedback>
+            </View>
+            <Text style={styles.name}>{name}</Text>
             <View style={styles.wrapper}>
                 <AppCardStatsItem
                     label="Сыграно игр"
@@ -43,14 +58,14 @@ const styles = StyleSheet.create({
     cardStats: {
         marginTop: 43
     },
-    userIcon: {
+    avatar: {
         width: 45,
         height: 45,
         borderRadius: 100,
         position: 'absolute',
         top: '-12%'
     },
-    userName: {
+    name: {
         marginTop: 15,
         fontFamily: fonts.semiBold,
         fontSize: 18,
---
+++ src/utils/avatars.js
@@ -0,0 +1,4 @@
+export const avatars = {
+    [0]: require('../assets/images/main/avatars/man.png'),
+    [1]: require('../assets/images/main/avatars/woman.png')
+}
