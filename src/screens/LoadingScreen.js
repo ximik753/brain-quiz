537e42ce52e9de614a15fab6dea12936634c2c86
ximik_753 <sankovdiman@mail.ru>
2020-10-15 21:10:54 +0300

    Add logo animation on boot

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import { View, StyleSheet, Text, Image } from 'react-native'
+import React, { useEffect, useRef } from 'react'
+import { View, StyleSheet, Text, Animated } from 'react-native'
 import { colors } from '../utils/colors'
 import { fonts } from '../utils/fonts'
 
 const LoadingScreen = () => {
+    const valueScale = useRef(new Animated.Value(1)).current
+    const valueOpacity = useRef(new Animated.Value(0)).current
+
+    const scaleAnimation = () => {
+        Animated.timing(valueScale, {
+            toValue: 1.1,
+            duration: 3000,
+            useNativeDriver: true
+        }).start()
+    }
+
+    const opacityAnimation = () => {
+        Animated.timing(valueOpacity, {
+            toValue: 1,
+            duration: 3000,
+            useNativeDriver: true
+        }).start()
+    }
+
+    useEffect(() => {
+        scaleAnimation()
+        opacityAnimation()
+    }, [])
+
     return (
         <View style={styles.wrapper}>
-            <Image
-                style={styles.logo}
+            <Animated.Image
+                style={[styles.logo, {
+                    transform: [{ scaleX: valueScale }, { scaleY: valueScale }],
+                    opacity: valueOpacity
+                }]}
                 source={require('../assets/images/loading/logo.png')}
             />
             <Text style={styles.production}>Production by Dev Team</Text>
