9c9746a870ac7c32db68d456262767cb6733feff
ximik_753 <sankovdiman@mail.ru>
2020-10-20 17:24:05 +0300

    Fix bugs in some devices, code optimization

--- src/components/AuthContainer.js
+++ src/components/AuthContainer.js
@@ -1,46 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import { View, StyleSheet, KeyboardAvoidingView, Image, Text, Keyboard } from 'react-native'
+import React from 'react'
+import { View, StyleSheet, ScrollView, Image, Text } from 'react-native'
 import AppButton from './UI/AppButton'
 import { colors } from '../utils/colors'
 import { fonts } from '../utils/fonts'
 
-const AuthContainer = ({ children, textButton, pressHandler, styleInputsWrapper, verticalOffset, isValid, loading }) => {
-    const [keyboardOpen, setKeyboardOpen] = useState(false)
-
-    const handleKeyboardShowToggle = () => setKeyboardOpen(prevState => !prevState)
-    useEffect(() => {
-        Keyboard.addListener('keyboardDidShow', handleKeyboardShowToggle)
-        Keyboard.addListener('keyboardDidHide', handleKeyboardShowToggle)
-
-        return () => {
-            Keyboard.removeListener('keyboardDidShow', handleKeyboardShowToggle)
-            Keyboard.removeListener('keyboardDidHide', handleKeyboardShowToggle)
-        }
-    })
+const AuthContainer = ({ children, textButton, pressHandler, styleInputsWrapper, isValid, loading }) => {
 
     return (
         <View style={styles.wrapper}>
-            <KeyboardAvoidingView
-                behavior="position"
-                keyboardVerticalOffset={verticalOffset || -60}
+            <ScrollView
+                contentContainerStyle={styles.container}
+                showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}
             >
-                <View style={styles.container}>
-                    <Image source={require('../assets/images/authorization/logo.png')}/>
-                    <Text style={styles.title}>Онлайн виктрина для прокачки своих знаний</Text>
-                    <View
-                        style={{ ...styles.inputsWrapper, ...styleInputsWrapper }}
-                    >
-                        {children}
-                    </View>
-                    <AppButton
-                        text={textButton}
-                        styleButton={keyboardOpen ? styles.buttonKeyboardOpen : styles.button}
-                        pressHandler={pressHandler}
-                        disabled={!isValid}
-                        loading={loading}
-                    />
+                <Image source={require('../assets/images/authorization/logo.png')}/>
+                <Text style={styles.title}>Онлайн виктрина для прокачки своих знаний</Text>
+                <View
+                    style={{ ...styles.inputsWrapper, ...styleInputsWrapper }}
+                >
+                    {children}
                 </View>
-            </KeyboardAvoidingView>
+                <AppButton
+                    text={textButton}
+                    styleButton={styles.button}
+                    pressHandler={pressHandler}
+                    disabled={!isValid}
+                    loading={loading}
+                />
+            </ScrollView>
         </View>
     )
 }
@@ -49,13 +36,10 @@ const styles = StyleSheet.create({
     wrapper: {
         flex: 1,
         alignItems: 'center',
-        backgroundColor: colors.background.default,
-        position: 'relative'
+        backgroundColor: colors.background.default
     },
     container: {
         maxWidth: 290,
-        width: '100%',
-        height: '100%',
         alignItems: 'center',
         paddingTop: 45
     },
@@ -72,9 +56,7 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between'
     },
     button: {
-        position: 'absolute',
-        bottom: 35,
-        left: 0
+        marginTop: 80
     },
     buttonKeyboardOpen: {
         marginTop: 10
