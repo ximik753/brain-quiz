bee63d48961005f2eebb867e9a0de62bfc842c2f
ximik_753 <sankovdiman@mail.ru>
2020-10-25 11:49:46 +0300

    Refinement of timer logic

--- src/components/Quiz/QuizTimer.js
+++ src/components/Quiz/QuizTimer.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 import { Circle, Svg, G } from 'react-native-svg'
 import { fonts } from '../../utils/fonts'
 import { colors } from '../../utils/colors'
+import { useSelector } from 'react-redux'
 
 const QuizTimer = () => {
+    const question = useSelector(state => state.game.question)
     const height = 45
     const width = 45
     const radius = (width - 10) / 2
     const circumference = radius * 2 * Math.PI
-    const progress = circumference - (30 / 100) * circumference
     const strokeWidth = 5
+    const [progress, setProgress] = useState(100)
+
+    useEffect(() => {
+        setProgress(100)
+        if (question) {
+            const id = setInterval(() => {
+                setProgress(prevState => {
+                    if (prevState === 0) {
+                        clearInterval(id)
+                        return 0
+                    }
+
+                    return prevState - 10
+                })
+            }, 1000)
+
+            return () => clearInterval(id)
+        }
+    }, [question])
 
     return (
         <View style={styles.wrapper}>
@@ -29,26 +49,26 @@ const QuizTimer = () => {
                         strokeWidth={strokeWidth}
                         stroke={colors.border.progress}
                         strokeDasharray={circumference}
-                        strokeDashoffset={progress}
+                        strokeDashoffset={circumference - (progress / 100) * circumference}
                     />
                 </G>
             </Svg>
-            <Text style={styles.timer}>7</Text>
+            <Text style={styles.timer}>{progress / 10}</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     wrapper: {
-        position: 'relative'
+        position: 'relative',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     timer: {
         fontFamily: fonts.bold,
         color: colors.font.default,
         fontSize: 18,
-        position: 'absolute',
-        left: '38%',
-        top: '23%'
+       position: 'absolute'
     }
 })
 
